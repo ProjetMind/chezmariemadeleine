a6c47dcc8efb88e4c05d13106262c8375a500565
nsi <nsi@10.0.34.1>
2014-03-08 18:16:28 +0100

    - Création du controller SiteController et suppression du controller par defaut - Creation de la route holme page du site

---
+++ src/Cmm/SiteBundle/Controller/SiteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Cmm\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SiteController extends Controller
+{
+    public function indexAction()
+    {
+    }
+}
