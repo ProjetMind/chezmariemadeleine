f3d2b2088b466303b9aef4be144d5860671c524e
nsi <nsi@10.0.32.212>
2014-03-12 16:02:10 +0100

    installation de fos js routing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Cmm\UserBundle\CmmUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
