9ce6f0b9c50f76fe67c9e219628206c7f43ebf12
nsi <nsi@192.168.1.5>
2014-03-20 02:42:58 +0100

    Mis en place du fond du site

--- src/Cmm/SiteBundle/Controller/SiteController.php
+++ src/Cmm/SiteBundle/Controller/SiteController.php
@@ -17,4 +17,56 @@ class SiteController extends Controller
         $template           = '::accueil.html.twig';
         return $this->container->get('templating')->renderResponse($template);
     }
+    
+    public function pagesAction(){
+        
+        $routeName          = $this->get('request')->get('_route');
+        $template           = '::accueil.html.twig';
+        
+        // Créer un services qui gère le switch
+        switch ($routeName){
+            
+            case 'cmm_site_page_creperie':
+                $template   = 'creperie';
+                break;
+            
+            case "cmm_site_page_presentation":
+                $template   = 'presentation';
+                break;
+            
+            case 'cmm_site_page_quartier':
+                $template   = 'quartier';
+                break;
+            
+            case 'cmm_site_page_en_ce_moment':
+                $template   = 'moment';
+                break;
+            
+            case 'cmm_site_page_produits':
+                $template   = 'produits';
+                break;
+            
+            case 'cmm_site_page_carte':
+                $template   = 'carte';
+                break;
+            
+            case 'cmm_site_page_crepe_galette':
+                $template   = 'crepes_galettes';
+                break;
+            
+            case 'cmm_site_page_formules':
+                $template   = 'formules';
+                break;
+            
+            case 'cmm_site_page_acces':
+                $template   = 'acces';
+                break;
+            
+            case 'cmm_site_page_contact':
+                $template   = 'conatact';
+                break;
+        }
+        
+        return $this->container->get('templating')->renderResponse($template);
+    }
 }
